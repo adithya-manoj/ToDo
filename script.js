030be717adeef4f995d660330fdb0cf336225380
adithya-manoj <adithyamanojv@gmail.com>
2024-04-03 23:46:46 +0530

    Updated with Checking if a blank task is added.

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-let todo=()=>{
-        let check=document.createElement('input')
-        check.type = 'checkbox';
-        
-        let value=document.getElementById('item').value
-        let element=document.createElement('li')
-        let text=document.createTextNode(value)
-        
-        element.appendChild(check)
-        element.appendChild(text)
+let todo = () => {
+    let value = document.getElementById('item').value.trim(); 
 
-        
-        let close=document.createElement('i')
-        close.className='trash fa fa-trash'
-        
-        element.appendChild(close)
-
-        element.className='list'
-
-        let container=document.getElementById('containerbody')
-        containerbody.appendChild(element);
-
-        close.addEventListener('click',()=>{
-            element.remove()
-        })
-        document.getElementById('item').value=''
+    if (value === '') {
+        alert('Please enter a Task !');
+        return; 
     }
+
+    let check = document.createElement('input');
+    check.type = 'checkbox';
+
+    let element = document.createElement('li');
+    let text = document.createTextNode(value);
+
+    element.appendChild(check);
+    element.appendChild(text);
+
+    let close = document.createElement('i');
+    close.className = 'trash fa fa-trash';
+
+    element.appendChild(close);
+
+    element.className = 'list';
+
+    let container = document.getElementById('containerbody');
+    container.appendChild(element);
+
+    close.addEventListener('click', () => {
+        element.remove();
+    });
+
+    document.getElementById('item').value = '';
+};
